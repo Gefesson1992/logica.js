e6506e240479d7d4dab4b81b45b67bf86a8013b2
Gefesson Oliveira <gefessonsilvadeoliveira@gmail.com>
2026-01-19 22:02:47 -0300

    atualização

---
+++ variaveis/login/script.js
@@ -0,0 +1,12 @@
+console.log('login');
+
+
+
+console.log(emailImput);
+console.log(senhaImput);
+
+function login() {
+    const emailImput = document.getElementById('email').value;
+    const senhaImput = document.getElementById('senha').value;
+    console.log('Clicou no login', emailImput, senhaImput);
+}
